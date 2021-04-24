35bd90c2ced58b65c2dd62a1625e134220715054
gglin001 <guosonglin001@qq.com>
2021-04-24 14:12:35 +0800

    [Misc] format code

--- scripts/onnx_shape_infer.py
+++ scripts/onnx_shape_infer.py
@@ -4,17 +4,22 @@ import onnx
 import onnx.shape_inference
 
 
-def onnx_shape_infer(fp):
-    output = f"{fp}_shape_infer.onnx"
-    onnx_model = onnx.load(fp)
+def onnx_shape_infer(args):
+    onnx_model = onnx.load(args.model)
     onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
-    onnx.save(onnx_model, output)
-    print(f"{output}, saved")
+    onnx.save(onnx_model, args.output)
+    print(f"{args.output}, saved")
 
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser()
-    parse.add_argument("--input", required=True, type=str)
-    args = parse.parse_args()
+    parse.add_argument(
+        "--model",
+        # required=True,
+        type=str,
+    )
+    _args = parse.parse_args()
+    # _args.model = f"/home/allen/ml_data/models/onnx/resnet18-v2-7.onnx"
+    _args.output = f"{_args.model}_shape_infer.onnx"
 
-    onnx_shape_infer(args.input)
+    onnx_shape_infer(_args)
